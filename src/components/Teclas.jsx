dd5963d12d34f5fbc36da7df9c3a4ec5a896a848
Ysnaldo Lopez <ysnaldolopez@gmail.com>
2021-05-17 18:25:30 -0500

    cambios nuevos

--- src/components/Teclas.jsx
+++ src/components/Teclas.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 // import React from 'react'
 import { Col, Container, Row, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faGithub, faTwitter, faWhatsapp, faGoogle } from '@fortawesome/free-brands-svg-icons';
 import styled from 'styled-components'
 import './teclas.css'
 import { sonidos } from '../utils/sonidos';
@@ -14,6 +16,7 @@ const StyledContainer = styled(Container)`
     /* width: 400px; */
     font-family: 'Noto Sans TC', sans-serif;
     border-radius: 10px;
+    flex-direction: column;
 `
 
 
@@ -21,7 +24,6 @@ const StyledButtomContainer = styled.button`
     display: flex; 
     justify-content: center; 
     align-items: center; 
-    background-color: aqua;
     width: 70px;
     height: 56px;
     margin: 5px;
@@ -35,45 +37,97 @@ function reproducir(section) {
     audio.play();
 }
 
+const StyledDisplayContainer = styled(Container)`
+    background-color: #344fa1;
+    margin-top: 20px;
+    text-align: center; 
+    width: 50%;
+    padding: 20px;
+    border-radius: 10px; 
+    color: white; 
+
+`
+
+const StyledHeader = styled(Container)`
+    background: #0E1C36;
+    color: white; 
+    padding: 20px; 
+    position: fixed; 
+    top: 0;
+    width: 100%;
+    font-weight: bold; 
+    left: 0; 
+`;
+
+const StyledFooterContainer = styled(Container)`
+    background: #0E1C36;
+    padding: 20px; 
+    color: white; 
+    position: fixed; 
+    bottom: 0; 
+    width: 100%;
+    text-align: center; 
+    font-weight: bold; 
+    left: 0; 
+`
+
 
 document.addEventListener("keydown", function (event) {
-
+    let sonidoMensaje;
     switch (event.key) {
         case 'q':
             document.querySelector('#Heater-1').style.backgroundColor = 'red';
             reproducir(document.getElementById(event.key.toUpperCase()));
+            sonidoMensaje = document.getElementById('display');
+            sonidoMensaje.textContent = 'Heater-1';
             break;
         case 'w':
             document.querySelector('#Heater-2').style.backgroundColor = 'red';
             reproducir(document.getElementById(event.key.toUpperCase()));
+            sonidoMensaje = document.getElementById('display');
+            sonidoMensaje.textContent = 'Heater-2';
             break;
         case 'e':
             document.querySelector('#Heater-3').style.backgroundColor = 'red';
             reproducir(document.getElementById(event.key.toUpperCase()));
+            sonidoMensaje = document.getElementById('display');
+            sonidoMensaje.textContent = 'Heater-3';
             break;
         case 'a':
             document.querySelector('#Heater-4').style.backgroundColor = 'red';
             reproducir(document.getElementById(event.key.toUpperCase()));
+            sonidoMensaje = document.getElementById('display');
+            sonidoMensaje.textContent = 'Heater-4';
             break;
         case 's':
             document.querySelector('#Clap').style.backgroundColor = 'red';
             reproducir(document.getElementById(event.key.toUpperCase()));
+            sonidoMensaje = document.getElementById('display');
+            sonidoMensaje.textContent = 'Clap';
             break;
         case 'd':
             document.querySelector('#Open-HH').style.backgroundColor = 'red';
             reproducir(document.getElementById(event.key.toUpperCase()));
+            sonidoMensaje = document.getElementById('display');
+            sonidoMensaje.textContent = 'Open-HH';
             break;
         case 'z':
             document.querySelector('#Kick-n-Hat').style.backgroundColor = 'red';
             reproducir(document.getElementById(event.key.toUpperCase()));
+            sonidoMensaje = document.getElementById('display');
+            sonidoMensaje.textContent = 'Kick-n-Hat';
             break;
         case 'x':
             document.querySelector('#Kick').style.backgroundColor = 'red';
             reproducir(document.getElementById(event.key.toUpperCase()));
+            sonidoMensaje = document.getElementById('display');
+            sonidoMensaje.textContent = 'Kick';
             break;
         case 'c':
             document.querySelector('#Closed-HH').style.backgroundColor = 'red';
             reproducir(document.getElementById(event.key.toUpperCase()));
+            sonidoMensaje = document.getElementById('display');
+            sonidoMensaje.textContent = 'Closed-HH';
             break;
         default:
             console.log("")
@@ -120,11 +174,9 @@ document.addEventListener("keyup", function (event) {
 
 // Controlar con evento de click
 function dispararSonido(e) {
-    // e.target.firstElementChild.play();
-    // console.log(e.target.id);
-    // document.querySelector('e.target.id').style.backgroundColor = 'red'
     reproducir(document.getElementById(e.target.firstElementChild.id));
-
+    let sonidoMensaje = document.getElementById('display');
+    sonidoMensaje.textContent = e.target.id;
 }
 
 export default function Teclas() {
@@ -135,18 +187,31 @@ export default function Teclas() {
     return (
         <React.Fragment>
             <Container fluid >
-                <StyledContainer id='drum-machine'>
-                    <Row id='display' style = {{background: '#77acf1', padding: '20px', borderRadius: '15px'}}>
+                <StyledHeader fluid>Drum Machine</StyledHeader>
+                <StyledContainer >
+                    <Row style={{ background: '#77acf1', padding: '20px', borderRadius: '15px' }} id='drum-machine'>
                         {sonidos.map(box =>
-                            <Col xs={4} style = {{display: 'flex', justifyContent: 'center'}}>
-                                <StyledButtomContainer className='drum-pad ' onClick={dispararSonido} id={box.id} style = {{color: 'white', fontWeight: 'bold', background: '#344fa1'}}>
+                            <Col xs={4} style={{ display: 'flex', justifyContent: 'center' }}>
+                                <StyledButtomContainer className='drum-pad ' onClick={dispararSonido} id={box.id} style={{ color: 'white', fontWeight: 'bold', background: '#344fa1' }}>
                                     {box.keyTrigger}
                                     <audio src={box.url} id={box.keyTrigger} className='clip' ></audio>
                                 </StyledButtomContainer>
                             </Col>
                         )}
                     </Row>
+                    <StyledDisplayContainer id='display'></StyledDisplayContainer>
                 </StyledContainer>
+                <StyledFooterContainer fluid className = 'footer-container'>Desarrollado por Ysnaldo Jose Lopez Hernandez  |
+                    <a href="https://www.facebook.com/" target = '_blank'>
+                        <FontAwesomeIcon icon={faFacebook} />
+                    </a>
+                    <a href="https://github.com/YsnaldoXVen" target = '_blank'>
+                        <FontAwesomeIcon icon={faGithub} />
+                    </a>
+                    <a href="https://twitter.com/lopezh_yjose" target = '_blank'>
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </a>
+                </StyledFooterContainer>
             </Container>
         </React.Fragment>
     )
